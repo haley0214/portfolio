bde0788dcd22989508f6b9d11368f1af6f7ba21c
yukigondo <0214g.yuki@gmail.com>
2025-08-31 14:30:31 +0900

    全体的な修正

--- js/main.js
+++ js/main.js
@@ -1,37 +1,26 @@
-window.addEventListener('scroll', function() {
-  const header = document.querySelector('.header');
-  const scrollPosition = window.scrollY;
-  const threshold = 550; 
 
-  if (scrollPosition > threshold) {
-    header.classList.add('header-scrolled');
-  } else {
-    header.classList.remove('header-scrolled');
-  };
-});
+  // aboutセクションのアニメーション
+  document.addEventListener("DOMContentLoaded", () => {
+    const targets = document.querySelectorAll(".about_content");
 
-// aboutセクションのアニメーション
-document.addEventListener("DOMContentLoaded", () => {
-  const targets = document.querySelectorAll(".about_content");
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        // 画面に入ったら show
-        entry.target.classList.add("show");
-        entry.target.classList.remove("hidden");
-      } else {
-        // 画面から出たら hidden
-        entry.target.classList.remove("show");
-        entry.target.classList.add("hidden");
-      }
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // 画面に入ったら show
+          entry.target.classList.add("show");
+          entry.target.classList.remove("hidden");
+        } else {
+          // 画面から出たら hidden
+          entry.target.classList.remove("show");
+          entry.target.classList.add("hidden");
+        }
+      });
+    }, {
+      threshold: 0.2
     });
-  }, {
-    threshold: 0.2 
-  });
 
-  targets.forEach(target => {
-    target.classList.add("hidden"); 
-    observer.observe(target);
+    targets.forEach(target => {
+      target.classList.add("hidden");
+      observer.observe(target);
+    });
   });
-});
